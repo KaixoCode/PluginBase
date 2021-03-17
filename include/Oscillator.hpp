924bc254c7ee99d0583775a01cc41ccef4079fe5
KaixoCode <jeroenzwiers2@gmail.com>
2021-03-17 14:44:53 +0100

    Update Oscillator.hpp

--- include/Oscillator.hpp
+++ include/Oscillator.hpp
@@ -4,18 +4,21 @@
 class Oscillator
 {
 public:
-	float NextSample()
+	float NextSample(double phaseoffset = 0)
 	{
         double delta = frequency / sampleRate;
-        phase = std::fmod(1 + phase + delta, 1);
-        sample = wavetable(phase);
-        return sample;
+        phase = std::fmod(1 + phase + phaseoffset + delta, 1);
+        return wavetable(phase);
 	}
+
+    float Sample(double phaseoffset = 0)
+    {
+        return wavetable(std::fmod(1 + phase + phaseoffset, 1));
+    }
 
     double(*wavetable)(double) = [](double p) { return 2 * (-p + 0.5); };
 
-    double sample = 0;
+    double phase = 0;
 	double frequency = 0;
-	double phase = 0;
     double sampleRate = 48000;
 };
